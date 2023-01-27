95e80e2208dfa3f1fa18b6e723daad579b2fae05
Adhamzineldin <mohalya3@gmail.com>
2023-01-28 00:30:20 +0200

    try 2

---
+++ main.py
@@ -0,0 +1,47 @@
+import datetime as dt
+import json
+import os
+import random
+import smtplib
+
+from dotenv import load_dotenv
+
+##################### Extra Hard Starting Project ######################
+
+with open(
+        "F:/computer science/Python Course/Python-Course/Course projects/Birthday Wisher/Birthday Wisher (Day 32) start/birthdays.json",
+        "r") as file:
+    data = json.load(file)
+
+names = [name for name in data.keys()]
+
+
+def send_mail(name, mail):
+    letters = [
+        "F:/computer science/Python Course/Python-Course/Course projects/Birthday Wisher/Birthday Wisher (Day 32) start/letter_templates/letter_1.txt",
+        "F:/computer science/Python Course/Python-Course/Course projects/Birthday Wisher/Birthday Wisher (Day 32) start/letter_templates/letter_2.txt",
+        "F:/computer science/Python Course/Python-Course/Course projects/Birthday Wisher/Birthday Wisher (Day 32) start/letter_templates/letter_3.txt"]
+    letter_file = f"{random.choice(letters)}"
+    with open(letter_file, "r") as letter:
+        text = letter.read()
+        text = text.replace("[NAME]", name.title())
+    load_dotenv("F:/computer science/EnvironmentVariables/data.env")
+    with smtplib.SMTP(os.getenv("smtp"), int(os.getenv("port"))) as connection:
+        connection.starttls()
+        connection.login(os.getenv("my_email"), os.getenv("password"))
+        connection.sendmail(
+            from_addr=os.getenv("my_email"),
+            to_addrs=mail,
+            msg="Subject:Happy Birthday\n\n"
+                f"{text}"
+
+        )
+        print(f"Email Sent Successfully to {name}")
+
+
+now = dt.datetime.now()
+for name in names:
+    if data[name]["month"] == now.month and data[name]["day"] == now.day:
+        send_mail(name, data[name]["email"])
+
+# python -m PyInstaller --windowed --onefile  main.py
